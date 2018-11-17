c1c7f10db045a989fc7b643f93785e96d282c79e
trinkmuetze <michelle.vorwerk@fh-bielefeld.de>
2018-11-17 20:33:29 +0100

    parse Class and Relationship in structs

---
+++ src/parser/uml_class.rs
@@ -0,0 +1,34 @@
+#[derive(Clone, Debug)]
+pub struct Class {
+    pub name: String,
+    pub attributes: Vec<Attribute>,
+    pub methods: Vec<Method>,
+}
+
+#[derive(Clone, Debug)]
+pub struct Attribute {
+    pub name: String,
+    pub visibility: String,
+    pub data_type: String,
+}
+
+#[derive(Clone, Debug)]
+pub struct Method {
+    pub name: String,
+    pub visibility: String,
+    pub return_type: String,
+    pub parameters: Vec<Parameter>,
+}
+
+#[derive(Clone, Debug)]
+pub struct Parameter {
+    pub name: String,
+    pub data_type: String,
+}
+
+#[derive(Clone, Debug)]
+pub struct Relationship {
+    pub relation_type: String,
+    pub class: String,
+    pub to_class: String,
+}
