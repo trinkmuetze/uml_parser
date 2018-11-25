8e84eee5fe79d9e1721f31bf92b3e0dddabc1115
trinkmuetze <michelle.vorwerk@fh-bielefeld.de>
2018-11-25 15:30:50 +0100

    Parameter Abfrage

--- src/main.rs
+++ src/main.rs
@@ -1,55 +1,34 @@
+use std::env;
+
 pub mod diagramVisualizer;
 pub mod parser;
 
 fn main() {
-    let valid = parser::validate_xml("BeispielXML.xml".to_string());
+    let args: Vec<String> = env::args().collect();
+    let mut valid = false;
 
-    if valid {
-        let data = parser::parse_data("BeispielXML.xml".to_string());
-        let packages = parser::get_packages(data.clone());
+    if args.len() == 2  {
+        let filename = &args[1];
+        valid = parser::validate_xml(filename.to_string());
 
-        for package in packages {
-            if diagramVisualizer::generateDiagram(package.classes, 720, 1280, "Test") {
-                println!("Diagramm erfolgreich erstellt!");
+        if valid {
+            let data = parser::parse_data(filename.to_string());
+            let packages = parser::get_packages(data.clone());
+
+            for package in packages {
+                if diagramVisualizer::generateDiagram(package.classes, 720, 1280, "Test") {
+                    println!("Diagram created!");
+                }
             }
+        } else {
+            println!("XML not valid or not found.");
         }
-    } else {
-        println!("XML not valid");
+    }
+    else if args.len() < 2 {
+        println!("Too few parameters.");
+    }
+    else if args.len() > 2 {
+        println!("Too much parameters.");
     }
 
-    /*//Example class
-    let mut attribute1 = String::from("- size");
-    let mut attribute2 = String::from("+ name");
-    let mut method1 = String::from("+ bark()");
-    let mut method2 = String::from("+ eat()");
-    let mut method3 = String::from("+ fly()");
-
-    let mut attributes = Vec::new();
-    attributes.push(attribute1);
-    attributes.push(attribute2);
-    let mut methods = Vec::new();
-    methods.push(method1);
-    methods.push(method2);
-    methods.push(method3);
-    let mut classes = Vec::new();
-    let class = Class{name: "OneClass".to_string(), attributes: attributes, methods: methods};
-
-    let mut attribute3 = String::from("- size");
-    let mut attribute4 = String::from("+ name");
-    let mut method4 = String::from("+ bark()");
-    let mut method5 = String::from("+ eat()");
-    let mut method6 = String::from("+ fly()");
-
-    let mut attributes2 = Vec::new();
-    attributes2.push(attribute3);
-    attributes2.push(attribute4);
-    let mut methods2 = Vec::new();
-    methods2.push(method4);
-    methods2.push(method5);
-    methods2.push(method6);
-    let class2 = Class{name: "TwoClass".to_string(), attributes: attributes2, methods: methods2};
-    classes.push(class);
-    classes.push(class2);
-
-    generateDiagram(classes,720,1280, "My First Diagram!");*/
 }
