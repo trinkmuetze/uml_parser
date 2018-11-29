1301e3934046cedda2d90f6d83bfd7c4fe97bd2d
trinkmuetze <michelle.vorwerk@fh-bielefeld.de>
2018-11-29 02:39:51 +0100

    main fixes

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,6 @@ fn main() {
     let args: Vec<String> = env::args().collect();
     let mut valid = true;
 
-<<<<<<< HEAD
     if args.len() == 3  {
         let uml_type = &args[1];
         let filename = &args[2];
@@ -29,28 +28,13 @@ fn main() {
             if valid {
                 let data = parser::parse_data(filename.to_string());
                 let packages = parser::get_packages(data.clone());
+                let relationships = parser::get_relationships(data.clone());
 
                 for package in packages {
-                    if diagramVisualizer::generateDiagram(package.classes, 720, 1280, "Test") {
+                    if diagramVisualizer::generateDiagram(relationships.clone(), package.classes, 720, 1280, "Test") {
                         println!("Diagram created!");
                     }
-=======
-    if args.len() == 2  {
-        let filename = &args[1];
-        //valid = parser::validate_xml(filename.to_string());
-
-        if valid {
-            let data = parser::parse_data(filename.to_string());
-            let packages = parser::get_packages(data.clone());
-            let relationships = parser::get_relationships(data.clone());
-
-            for package in packages {
-                if diagramVisualizer::generateDiagram(relationships.clone(), package.classes, 720, 1280, "Test") {
-                    println!("Diagram created!");
->>>>>>> 733fef19f1f2bad9e3753aa37f939aa012127dd5
                 }
-            } else {
-                println!("XML not valid or not found.");
             }
         }
     }
